8e50d2af1c1e8ae5b6896dcbd64c7f0e19e5e5d9
Vinicius Fonseca <v.lara.fonseca@gmail.com>
2025-09-29 15:59:14 -0300

    Valores Iniciais mais condizentes, code e sampler precisam de alterações

--- ValoresIniciais.R
+++ ValoresIniciais.R
@@ -1,49 +1,87 @@
-# Definindo o número de Klusters (K)
-K = 4
+#ajustar o offset globalmente
+min_max_scale_matrix <- function(mat, a , b) {
+  scaled_mat <- mat  # Cria uma cópia da matriz original
+  
+  for (j in 1:ncol(mat)) {
+    min_val <- min(mat[, j])
+    max_val <- max(mat[, j])
+    # Aplica Min-Max Scaling para o intervalo [a, b]
+    scaled_mat[, j] <- a + ((mat[, j] - min_val) / (max_val - min_val)) * (b - a)
+  }
+  
+  return(scaled_mat)
+}
 
-# Estimando os coeficientes iniciais usando um modelo GLM (Modelo Linear Generalizado)
-# O modelo utiliza a variável resposta Y2009_2011 e preditores selecionados da matriz x.
+# Exemplo de uso
+# Supondo que 'my_matrix' seja a matriz que você deseja escalar
+  # Exemplo de matriz
+E <- min_max_scale_matrix(E,a=0.5,b=1.5)
+##Matriz de lambdas
+lambda_at = matrix(, ncol = 23, nrow = 75)
+lambda_smooth = matrix(, ncol = 23, nrow = 75)
+K = 4 #número de clusters
 n_regions = dim(E)[1]  # Número de regiões
 n_times = dim(E)[2]     # Número de períodos de tempo
-
-# Ajustar o modelo GLM com a função de distribuição Poisson e link log
-glm <- glm(Y2009_2011 ~ 1 + x[, 10, 1] + x[, 10, 2] + x[, 10, 3], family = poisson(link = "log"))
-
-# Exibir o resumo do modelo ajustado
+#valores iniciais
+y0 = Y1999_2001
+glm <- glm(y0~1+x[,1,1]+x[,1,2]+x[,1,3],family = poisson(link = "log"))
 summary(glm)
-
-# Extraindo o coeficiente intercepto (lambda0) do modelo
-lambda0 = glm$coefficients[1]
-print(lambda0)
-log(lambda0)
-# Extraindo os coeficientes beta iniciais (para as variáveis preditoras)
+mu_lambda0 = glm$coefficients[1]
+var_lambda0 = 2
+#calculo a0 e b0 a partir do glm e variancia arbirtraria
+b0 = mu_lambda0/var_lambda0
+a0= mu_lambda0*(b0^2)
+#Definindo valores iniciais de beta a partir do glm
 beta_ini = glm$coefficients[2:4]
-print(beta_ini)
+#número de covariaveis
+p = length(beta_ini)
+#matriz da média da poisson Y
+mu_at <- matrix(nrow = n_regions, ncol = n_times)
+mu_smooth <- matrix(nrow = n_regions, ncol = n_times)
+y_smooth <- matrix(nrow = n_regions, ncol = n_times)
+#matriz de Y
+y_at <- matrix(,nrow = n_regions, ncol = n_times+1)
+y_at[,1]<-y0
+#Matriz de att_ini,btt_ini,at_ini,bt_ini
+att_ini <- matrix(,nrow = n_regions, ncol = n_times)
+btt_ini <- matrix(,nrow = n_regions, ncol = n_times)
+at_ini  <- matrix(,nrow = n_regions, ncol = n_times+1)
+bt_ini  <- matrix(,nrow = n_regions, ncol = n_times+1)
 
-# Definindo valores iniciais para os parâmetros a e b
-a0 = 1  # Parâmetro a inicial
-b0 = 1  # Parâmetro b inicial
-
+#w arbitrário
+w = 0.9
 # Definindo valores iniciais para os parâmetros gamma
 gamma_ini = c(0.05, 0.10, 0.10, 0.15)
-
-# Inicializando os parâmetros a e b para uma distribuição uniforme
-a_unif_ini = c(0.00, rep(0, K - 1))  # Parâmetros a uniformes
-b_unif_ini = c(0.1, rep(1, K - 1))   # Parâmetros b uniformes
-
-# Estimando epsilon com base em um fator hAI e os parâmetros gamma
+#calculo epsilons iniciais
 epsilon_ini = 1 - hAI %*% gamma_ini
-# Expandindo epsilon para uma matriz com 75 regiões e 23 períodos
-epsilon_ini = matrix(rep(epsilon_ini, 23), nrow = 75, ncol = 23)
-
-# Estimando lambda inicial utilizando a distribuição gamma
-lambda_ini = matrix(rgamma(1725, shape = a0, rate = b0), ncol = 23, nrow = 75)
-
-# Estimando Y com base nos parâmetros lambda, E e epsilon, aplicando a função exponencial nos coeficientes beta
-mu_ini = lambda_ini * E * epsilon_ini * exp(beta_ini[1] * x[,, 1] + beta_ini[2] * x[,, 2] + beta_ini[3] * x[,, 3])
-y_ini = matrix(rpois(1725,mu_ini),nrow = n_regions,ncol = n_times)
-#estimando o theta com os betas iniciais e o x
-theta_ini = exp(beta_ini[1] * x[,, 1] + beta_ini[2] * x[,, 2] + beta_ini[3] * x[,, 3])
-# Definindo um parâmetro w para ser utilizado em cálculos posteriores
-
-w=0.7
+#CALCULO att_ini,btt_ini,at_ini,bt_ini
+for(i in 1:n_regions) {
+  at_ini[i,1] <- a0
+  bt_ini[i,1] <- b0
+  for(t in 2:(n_times+1)) {
+    att_ini[i,t-1] <- w * at_ini[i,t-1]
+    #cat("Processing region:", i, "at time:", t - 1, "with att:", att_ini[i,t-1], "\n")
+    btt_ini[i,t-1] <- w * bt_ini[i,t-1] #/ (epsilon_ini[i] * E[i,t-1] * exp((x[i,t-1,1:p] %*% beta_ini)))
+    #cat("Processing region:", i, "at time:", t - 1, "with btt:", btt_ini[i,t-1], "\n")
+    at_ini[i,t] <- att_ini[i,t-1] + y_at[i,t-1]
+    #cat("Processing region:", i, "at time:", t - 1, "with at:", at_ini[i,t], "\n")
+    bt_ini[i,t] <- btt_ini[i,t-1] + (1) * exp((x[i,t-1,1:p] %*% beta_ini)) * epsilon_ini[i] * E[i,t-1]
+    #cat("Processing region:", i, "at time:", t - 1, "with bt:", bt_ini[i,t], "\n")
+    lambda_at[i,t-1] <- rgamma(1,shape = att_ini[i,t-1],rate = btt_ini[i,t-1])
+    cat("Processing region:", i, "at time:", t - 1, "with lambda_at:", lambda_at[i,t-1], "\n")
+    mu_at[i,t-1] <- lambda_at[i,t-1] * exp((x[i,t-1,1:p]%*%beta_ini)) * epsilon_ini[i]*E[i,t-1]
+    #cat("Processing region:", i, "at time:", t - 1, "with mu:", mu_at[i,t-1], "\n")
+    y_at[i,t] <- rpois(1,mu_at[i,t-1])
+  }}
+lambda_smooth[,n_times] <-lambda_at[,n_times]
+y_smooth[,n_times] <- y_at[,n_times]
+mu_smooth[,n_times] <- mu_at[,n_times]
+for(t in n_times:2){
+  lambda_smooth[,t-1] <- rgamma(75,shape = (1-w)*at_ini[,t-1],rate = bt_ini[,t-1])+w*lambda_smooth[,t]
+  mu_smooth[,t-1] <- lambda_smooth[,t-1] * exp((x[,t-1,1:p]%*%beta_ini))*epsilon_ini*E[,t-1]
+  y_smooth[,t-1] <- rpois(75,mu_smooth[,t-1])
+}
+y_ini = y_smooth
+lambda_ini = lambda_smooth
+a_unif_ini = c(0.00, rep(0, K - 1))  # Parâmetros a uniformes
+b_unif_ini = c(0.1, rep(0.99, K - 1)) 
